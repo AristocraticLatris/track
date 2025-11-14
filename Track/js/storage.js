fca31d54a4e86e4273c1a1c55c2e68bc0f9dd901
ArystoTech <adeleyedivine776@gmail.com>
2025-11-14 21:05:40 +0100

    Delete Track directory

--- Track/js/storage.js
+++
@@ -1,8 +0,0 @@
-const STORAGE_KEY='track-timetable';
-
-function saveTimetable(tt){ localStorage.setItem(STORAGE_KEY,JSON.stringify(tt)); }
-
-function loadTimetable(){
-  const data=localStorage.getItem(STORAGE_KEY);
-  return data?JSON.parse(data):{ monday:[], tuesday:[], wednesday:[], thursday:[], friday:[], saturday:[], sunday:[] };
-}
